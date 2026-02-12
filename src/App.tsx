5d56a30ad4780e6a36fa4cef29e99f99e0a8af4c
MUKESH PAL <133365427+mukprabhakar@users.noreply.github.com>
2026-02-12 23:29:53 +0530

    Configure for custom domain rbstourandtravels.in with proper base path and CNAME

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/RBS-Tours-and-Travel">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<ServicesPage />} />
